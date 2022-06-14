c7038fa35a2d2e3cfff9bd4b68b8a3a458f18b44
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-13 18:50:25 -0700

    user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,10 +2,88 @@ const { User, Thought } = require("../models");
 
 const userController = {
   // GET all users
+  getUsers(req, res) {
+    User.find()
+      .select("-__v")
+      .then((dbUserData) => {
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
   // GET single user by _id && thought and friend data
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .populate("friends")
+      .populate("thoughts")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "No user with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
   // POST new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => {
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
   // PUT update user by _id
-  // DELETE remove iser by _id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      {
+        $set: req.body,
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "No user with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // DELETE remove user by _id
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "No user with this id!" });
+        }
+      })
+      .then(() => {
+        res.json({ message: "User deleted!" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -13,6 +13,6 @@ const {
 // GET single user by _id && thought and friend data
 // POST new user
 // PUT update user by _id
-// DELETE remove iser by _id
+// DELETE remove user by _id
 
 module.exports = router;
