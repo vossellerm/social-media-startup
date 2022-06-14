a00f16f3fe7080580beba145afc3beb6f272ea05
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-13 18:31:39 -0700

    User model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,52 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema();
-// username: String, unique, required, trimmed
-// email: string, required, unique, must match valid email address
-// thoughts: array of _id, reference Thought model
-// friends: array of _id, reference User model
+const userSchema = new Schema(
+  {
+    // username: String, unique, required, trimmed
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    // email: string, required, unique, must match valid email address
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+
+    // thoughts: array of _id, reference Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    // friends: array of _id, reference User model
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  // toJSON
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 // friendCount virtual: retrieves length of friends array
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
 const User = model("User", userSchema);
 
