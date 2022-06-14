4a9da18bf389365ce156e13497375a95c21c84f8
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-13 18:47:09 -0700

    user controller psuedocode

---
+++ controllers/user-controller.js
@@ -0,0 +1,11 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+  // GET all users
+  // GET single user by _id && thought and friend data
+  // POST new user
+  // PUT update user by _id
+  // DELETE remove iser by _id
+};
+
+module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,13 @@
 const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/user-controller");
 
 // GET all users
 // GET single user by _id && thought and friend data
