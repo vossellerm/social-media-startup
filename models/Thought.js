1472934a2ee7d5ed297c5102127e5821528d3d35
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-13 18:37:18 -0700

    Thoughts model

--- models/Thought.js
+++ models/Thought.js
@@ -2,15 +2,43 @@ const { Schema, model } = require("mongoose");
 const reactionSchema = require("./Reaction");
 const dateFormat = require("../utils/dateFormat");
 
-const thoughtSchema = new Schema();
+const thoughtSchema = new Schema(
+  {
+    // thoughtText: string, required, between 1 and 280 characters
+    thoughtText: {
+      type: String,
+      required: "You need to leave a thought!",
+      minlength: 1,
+      maxlength: 280,
+    },
 
-// thoughtText: string, required, between 1 and 280 characters
-// createdAt: Date, default is current timestamp, getter method format timestamp on query
-// username: string, required
-// reactions: array from reactionSchema
-// toJSON
+    // createdAt: Date, default is current timestamp, getter method format timestamp on query
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    // username: string, required
+    username: {
+      type: String,
+      required: true,
+    },
+    // reactions: array from reactionSchema
+    reactions: [reactionSchema],
+  },
 
+  // toJSON
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 // reactionCount virtual: retrieve lenght of thought's reactions array
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
 const Thought = model("Thought", thoughtSchema);
 
