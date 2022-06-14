5eeb5468eed6fb4cef59dae6312c001ab248d444
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-13 18:44:12 -0700

    user routes psuedocode

---
+++ routes/api/user-routes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+// GET all users
+// GET single user by _id && thought and friend data
+// POST new user
+// PUT update user by _id
+// DELETE remove iser by _id
+
+module.exports = router;
