a471ff6073cd00b5890436bbf787f9a20dd07d2b
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-13 18:41:26 -0700

    Reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,11 +1,41 @@
 const { Schema, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const reactionSchema = new Schema();
-// reactionId: ObjectId data type
-// reactionBody: String, required, 280 character max
-// username: string, required
-// createdAt: date, default current timestamp, getter method to format timestamp
-// toJSON
+const reactionSchema = new Schema(
+  {
+    // reactionId: ObjectId data type
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+
+    // reactionBody: String, required, 280 character max
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+
+    // username: string, required
+    username: {
+      type: String,
+      required: true,
+    },
+
+    // createdAt: date, default current timestamp, getter method to format timestamp
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  // toJSON
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 module.exports = reactionSchema;
